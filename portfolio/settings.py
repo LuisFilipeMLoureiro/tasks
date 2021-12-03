c07a04a719b168e121d7f9884cce1783b1a2aeca
Luís Filipe Martins Loureiro <luisfml@al.insper.edu.br>
2021-12-03 17:14:46 -0300

    update settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -41,11 +41,6 @@ INSTALLED_APPS = [
     'tasks.apps.TasksConfig',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
