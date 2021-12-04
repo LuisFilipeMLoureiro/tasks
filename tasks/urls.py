0ae2ac922c41a6e8ed3f7f4cceaa30078893977b
Luís Filipe Martins Loureiro <luisfml@al.insper.edu.br>
2021-12-03 21:10:22 -0300

    ..

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,15 +1,6 @@
-from django.urls import include, path
-from rest_framework import routers
-from . import views
-# from views import TaskViewSet
+from django.conf.urls import url 
+from tasks import views
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('tasks/<int:pk>', views.delete_task, name="delte"),
-    path('all_tasks/', views.get_all_tasks, name="all_task"),
-    path('post/', views.post_tasks, name="post"),
-    # path('post/', views.post_tasks, name="post_task"),
-    # path('delete/', views.delete_task, name="delete_task"),
+    url(r'^tasks/tasks$', views.tasks)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -13,27 +13,33 @@ from rest_framework.parsers import JSONParser
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
 
-@api_view(['GET'])
-def get_all_tasks(request):
-    all_tasks = Task.objects.all()
-    json_response = serializers.serialize("json", all_tasks)
-    return HttpResponse(json_response, content_type="application/json", status=status.HTTP_200_OK)
+@api_view(['GET', 'POST', 'DELETE'])
+def tasks(requisicao):
 
-@api_view(['POST'])
-def post_tasks(request):
-    serializer = TaskSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        # json_response = serializers.serialize("json", serializer)
-        return JsonResponse(serializer.data, status=201)
-    return JsonResponse(serializer.errors, status=400)
+    if requisicao.method == 'GET':
+        all_tasks = Task.objects.all()
+        json_response = serializers.serialize("json", all_tasks)
+        return HttpResponse(json_response, content_type="application/json", status=status.HTTP_200_OK)
 
-@api_view(['DELETE'])
-def delete_task(request, pk):
-    try:
-        task = Task.objects.get(pk=pk)
-        task.delete()
-    except Task.DoesNotExist:
-       return Response(status=status.HTTP_404_NOT_FOUND)
 
-    return JsonResponse({"message": "Task deletada com sucesso"}, status=status.HTTP_200_OK)
+    elif requisicao.method == 'POST':
+        serializer = TaskSerializer(data=requisicao.data)
+        if serializer.is_valid():
+            serializer.save()        
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if requisicao.method == 'DELETE':
+        tasks_deleted = Task.objects.all().delete()
+        return JsonResponse({'message': '{} Tutorials were deleted successfully!'.format(tasks_deleted[0])}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+    '''
+    Referencias:
+    [1] - https://www.bezkoder.com/django-postgresql-crud-rest-framework/
+    [2] - https://www.django-rest-framework.org/tutorial/quickstart/ 
+        
+    '''
