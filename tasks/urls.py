e4d749c6f13be207a2b2591bc56f890d7f016e9b
Luís Filipe Martins Loureiro <luisfml@al.insper.edu.br>
2021-12-03 22:35:44 -0300

    arrumando endpoints

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url 
 from tasks import views 
+from django.urls import path
  
 urlpatterns = [ 
-    url(r'^api/tasks$', views.ENDPOINTS),
+    path('tasks/<int:pk>', views.DELETE, name="delte"),
+    path('all_tasks/', views.GET, name="all_task"),
+    path('post/', views.POST, name="post"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,35 +1,38 @@
 from django.shortcuts import render
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse,Response
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from django.core import serializers
 
-
-
-@api_view(['GET', 'POST', 'DELETE'])
-def ENDPOINTS(request):
+#verbos https
+@api_view(['GET', 'POST'])
+def GET_POST(request):
 
     if request.method == 'GET':
-        t = Task.objects.all()
-        series = TaskSerializer(t, many=True)
-        
-        return JsonResponse(series.data, safe=False)
+        get = Task.objects.all()
+        response = serializers.serialize("json", get)
+        return HttpResponse(response, content_type="application/json", status=status.HTTP_200_OK)
 
-    elif request.method == 'POST':
-        t_post= JSONParser().parse(request)
-        series_post = TaskSerializer(data=t_post)
+    else:
+        if request.method == 'POST':
+            response_post = TaskSerializer(data=request.data)
+            if response_post.is_valid():
+                response_post.save()
+                return JsonResponse(response_post.data, status=status.HTTP_201_CREATED)
+            return JsonResponse(response_post.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        if series_post.is_valid():
-            series_post.save()            
-            return JsonResponse(series_post.data, status=status.HTTP_201_CREATED) 
-            
-        return JsonResponse(series_post.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['DELETE'])
+def DELETE(pk):
 
-    elif request.method == 'DELETE':
-        count = Task.objects.all().delete()
-        return JsonResponse({'message': '{} Tutorials were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
+    response_get = Task.objects.get(pk=pk)
+    response_get.delete()
+    return JsonResponse({"deletado!"}, status=status.HTTP_200_OK)
 
 
 
