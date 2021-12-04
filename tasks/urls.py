31b6b68ddad8becd21ae306d68ea4e0144e743e5
Luís Filipe Martins Loureiro <luisfml@al.insper.edu.br>
2021-12-03 22:36:31 -0300

    urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from tasks import views 
 from django.urls import path
  
-urlpatterns = [ 
-    path('tasks/<int:pk>', views.DELETE, name="delte"),
-    path('all_tasks/', views.GET, name="all_task"),
-    path('post/', views.POST, name="post"),
+urlpatterns = [
+    path("tasks/<int:pk>", views.DELETE, name="task"),
+    path("tasks/", views.GET_POST, name="all_task"),
+
+
 ]
